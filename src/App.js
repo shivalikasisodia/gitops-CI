1c8f163dd61b06d594d8476e65363985c6a71874
Shivalika <shivalika@gmail.com>
2024-01-08 10:41:15 +0530

    chngsee

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>changes applied</span>
+          Version <span style={{ color: 'red' }}>change</span>
         </div>
       </header>
     </div>
